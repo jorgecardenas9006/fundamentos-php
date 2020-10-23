d0691da3c2ec8bd48d93708a842b057ad1c92e78
jorgecardenas9006 <jorge.cardenas9006@gmail.com>
2020-10-22 23:06:36 -0500

    agregando funciones

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-echo upper('hola');
+echo upper('hola'."<br>");
+echo lower('hola');
--- src/helpers.php
+++ src/helpers.php
@@ -1,4 +1,12 @@
 <?php
-function upper($value){
-    return Text\Format::upperText($value);
+
+if (! funtion_exists('upper')) {
+    function upper($value){
+        return Text\Format::upperText($value);
+    }
+}
+if (! funtion_exists('lower')) {
+    function lower($value){
+        return Text\Format::lowerText($value);
+    }
 }
